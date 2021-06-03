eda638221d91df0ce5d8445dfdde0153cabfb7ae
Ciarm-nik <nikcia@libero.it>
2021-06-03 19:49:42 +0200

    Aggiunta sass, laravel mix e css

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 include "../php-ajax-dischi/database.php";
 
- var_dump($dischi);
+// var_dump($dischi);
 
 ?>
 
@@ -15,13 +15,35 @@ include "../php-ajax-dischi/database.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP-AJAX-DISCHI</title>
     <link rel="stylesheet" href="dist/app.css">
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 </head>
 
 <body>
 
     <header></header>
 
-    <main> </main>
+    <main>
+        <section class="container">
+            <div class="container dischi-box">
+                <?php foreach ($dischi as $disco) { ?>
+                    <div class="card">
+                        <div class="disco-content">
+                            <div class="poster-img">
+                                <img src="<?php echo $disco['poster'] ?>" alt="">
+                            </div>
+                            <h4><?php echo $disco['title'] ?></h4>
+                                <h5><?php echo $disco['author'] ?></h5>
+                                    <!-- <h2><?php echo $disco['genre'] ?></h2> -->
+                                        <p><?php echo $disco['year'] ?></p>
+                        </div>
+                    </div>
+                <?php
+                }
+                ?>
+            </div>
+        </section>
+
+    </main>
 
     <footer></footer>
 
